eb64368fa820cf017c09d4cf46b7e9a3a9dcd462
Thea <thea.joy@outlook.com>
2024-04-19 17:49:17 +0200

    feat: add Luciole font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title>{{ $title ?? config('app.name') }}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Reddit+Mono:wght@200..900&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
