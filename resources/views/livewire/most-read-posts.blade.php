9ea89edfa801a34c9342e52d405c6ca90a0f51c7
Thea <thea.joy@outlook.com>
2024-04-20 20:40:03 +0200

    feat: ordering posts

--- resources/views/livewire/most-read-posts.blade.php
+++ resources/views/livewire/most-read-posts.blade.php
@@ -6,7 +6,7 @@
     </h2>
 
     <div
-        class="grid grid-flow-col grid-cols-1 grid-rows-4 gap-4 gap-y-4 px-6 pt-6 md:grid-cols-2 md:grid-rows-2 md:space-y-0 lg:grid-cols-1 lg:grid-rows-4 lg:px-0 xl:grid-cols-2 xl:grid-rows-2"
+        class="grid gap-4 gap-y-4 px-6 pt-6 md:grid-cols-2 md:grid-rows-2 md:space-y-0 lg:grid-cols-1 lg:grid-rows-4 lg:px-0 xl:grid-cols-2 xl:grid-rows-2"
     >
         @foreach ($this->posts as $k => $post)
             <x-small-post-item :post="$post" />
