3335dc7b460b62f6e64181c97c5f0a44a272e2c0
Thea <thea.joy@outlook.com>
2024-09-19 22:02:06 +0200

    feat(feed): add rss feed

--- app/Domain/ValueObjects/PostIndex.php
+++ app/Domain/ValueObjects/PostIndex.php
@@ -4,8 +4,10 @@ namespace App\Domain\ValueObjects;
 
 use Carbon\Carbon;
 use Livewire\Wireable;
+use Spatie\Feed\Feedable;
+use Spatie\Feed\FeedItem;
 
-readonly class PostIndex implements Wireable
+readonly class PostIndex implements Wireable, Feedable
 {
     public function __construct(
         public PostId $postId,
@@ -21,20 +23,6 @@ readonly class PostIndex implements Wireable
         public ?string $alt,
         public Dimensions $dimensions,
     ) {
-    }
-    public function toLivewire(): array
-    {
-        return [
-            'id' => $this->postId->toLivewire(),
-            'title' => $this->title,
-            'slug' => $this->slug,
-            'description' => $this->description,
-            'date' => $this->date->toDateString(),
-            'reading' => $this->reading->toLivewire(),
-            'image' => $this->image,
-            'alt' => $this->alt,
-            'dimensions' => $this->dimensions->toLivewire(),
-        ];
     }
 
     public static function fromLivewire($value): PostIndex
@@ -55,6 +43,21 @@ readonly class PostIndex implements Wireable
         );
     }
 
+    public function toLivewire(): array
+    {
+        return [
+            'id' => $this->postId->toLivewire(),
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'date' => $this->date->toDateString(),
+            'reading' => $this->reading->toLivewire(),
+            'image' => $this->image,
+            'alt' => $this->alt,
+            'dimensions' => $this->dimensions->toLivewire(),
+        ];
+    }
+
     public function getImage(): string
     {
         return $this->image
@@ -62,5 +65,18 @@ readonly class PostIndex implements Wireable
                 ? $this->image
                 : "/$this->image"
             : '/covers/trans-pride.webp';
+    }
+
+    public function toFeedItem(): FeedItem
+    {
+        return FeedItem::create()
+            ->id($this->postId->value)
+            ->title($this->title)
+            ->summary($this->description)
+            ->updated($this->date)
+            ->link(route('posts.show', $this->slug))
+            ->authorName('Thea Cake')
+            ->category($this->category?->title)
+            ->image(route('home') . $this->getImage());
     }
 }
