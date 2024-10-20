a6de449455dc0e565ec84cda27a2112e5a88de69
Thea <115863819+cup-of-thea@users.noreply.github.com>
2024-10-20 22:58:25 +0200

    feat(posts): draft (#55)

---
+++ resources/views/filament/widgets/tools.blade.php
@@ -0,0 +1,7 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <x-filament::button wire:click="flushCache">
+            Flush cache
+        </x-filament::button>
+    </x-filament::section>
+</x-filament-widgets::widget>
