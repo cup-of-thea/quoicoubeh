b7a49b2868ca5ba90f8462d903904d2128ddd118
Thea <115863819+cup-of-thea@users.noreply.github.com>
2024-12-29 02:14:03 +0000

    feat(component): add gallery

---
+++ resources/views/components/gallery.blade.php
@@ -0,0 +1,8 @@
+<div role="list" class="gallery-container">
+	<div>
+		<img class="gallery-img" src="https://cupof.coffee/storage/OsDqpnH5GMjB1WcZy7jW4UoGv66LZ1rhGXjaCddE.png" alt="Une photo de moi maquillée devant une glace avec un haut blanc décolleté, une veste en jean, un jean et des gants">
+	</div>
+		<div>
+		<img class="gallery-img" src="https://cupof.coffee/storage/2c7126cKwB5oqcbSSLGyMBwTn7Aurpuigr1kCb0P.png" alt="Une photo de mon bureau amménagé de manière cosy pour lire avec des bougies et un espace où s'asseoir confortablement">
+	</div>
+</div>
