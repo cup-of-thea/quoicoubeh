fcd7162517388aca33b16681fbe7b3be291c6dfe
Thea <thea.milesi@agicap.com>
2025-01-02 23:02:12 +0100

    fix(font): remove thin font

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -2,7 +2,7 @@
     <div class="generic-component">
         <h2 class="text-2xl">Événements</h2>
         <p
-            class="mt-6 text-base font-thin leading-relaxed text-zinc-600 dark:text-zinc-400"
+            class="mt-6 text-base leading-relaxed text-zinc-600 dark:text-zinc-400"
         >
             Viens me retrouver lors de mes prochains événements, que ce soit en
             ligne ou IRL.
