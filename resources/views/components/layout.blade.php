e357afac2d748045bcbf1ab896af70adeed2d669
Thea <thea.joy@outlook.com>
2025-01-18 00:42:41 +0100

    feat(analytics): add pirsh

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8" />
-        <meta
+<head>
+    <meta charset="utf-8"/>
+    <meta
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
-        />
-        {{ $meta ?? "" }}
-        <title>{{ $title ?? config("app.name") }}</title>
-        @vite("resources/css/app.css")
-        @livewireStyles
-        <x-feed-links />
-    </head>
-    <body
+    />
+    {{ $meta ?? "" }}
+    <title>{{ $title ?? config("app.name") }}</title>
+    @vite("resources/css/app.css")
+    @livewireStyles
+    <x-feed-links/>
+    <script defer src="https://api.pirsch.io/pa.js"
+            id="pianjs"
+            data-code="RWUIFHQXceA9iy6QqvIbdom387a3t4hg"></script>
+</head>
+<body
         class="{{ Cache::get("theme:" . request()->ip(), "light") }} layout-body"
-    >
-        <livewire:navigation-top />
+>
+<livewire:navigation-top/>
 
-        <main class="layout-main">
-            {{ $slot }}
-        </main>
+<main class="layout-main">
+    {{ $slot }}
+</main>
 
-        @livewireScripts
-        <script>
-            document.addEventListener('livewire:init', () => {
-                Livewire.on('theme-changed', (event) => {
-                    document
-                        .querySelector('body')
-                        .classList.remove('light', 'dark');
-                    document.querySelector('body').classList.add(event);
-                });
-            });
-        </script>
-        {{ $scripts ?? "" }}
-    </body>
+@livewireScripts
+<script>
+    document.addEventListener('livewire:init', () => {
+        Livewire.on('theme-changed', (event) => {
+            document
+                .querySelector('body')
+                .classList.remove('light', 'dark');
+            document.querySelector('body').classList.add(event);
+        });
+    });
+</script>
+{{ $scripts ?? "" }}
+</body>
 </html>
