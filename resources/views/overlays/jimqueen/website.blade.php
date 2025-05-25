7e3ffdae16c628de72203fbfe9b9f6e5d34ccee5
Thea <115863819+cup-of-thea@users.noreply.github.com>
2025-05-25 17:49:21 +0200

    feat(overlays): add jim queen website overlay (#75)

---
+++ resources/views/overlays/jimqueen/website.blade.php
@@ -0,0 +1,9 @@
+<x-overlay>
+    <div class="jq-website flex items-center w-96 relative">
+        <img src="{{ asset('images/overlays/jimqueen/website.png') }}" alt="JimQueen Website Overlay"
+             class="w-64">
+        <p class="bg-white p-2 px-6 pt-4 rounded-md -ml-[10rem] mt-[4rem]">viens sur
+            <span class="text-[#9970e6]">https://jimqueen.fr/</span></p>
+    </div>
+
+</x-overlay>
